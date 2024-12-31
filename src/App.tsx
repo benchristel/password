72997e39a22e5dee89aa47f5368be9b4aef80b07
Ben Christel <ben.christel@gmail.com>
2024-12-31 12:36:14 -0700

    Delete example tests

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,3 @@ import "./app.css"
 export function App() {
     return <h1>Hello from Preact!</h1>
 }
-
-test("App", {
-    "renders a greeting"() {
-        expect(App().props.children, equals, "Hello from Preact!")
-    },
-})
